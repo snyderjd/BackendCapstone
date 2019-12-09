c9a22ececa29ecef63bff9bb0405ef651dae1418
Joseph Snyder <snyderjosephd@yahoo.com>
2019-12-09 11:30:16 -0600

    Add project files.

---
+++ PortfolioAnalyzer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PortfolioAnalyzer.Data;
+using PortfolioAnalyzer.Models;
+
+[assembly: HostingStartup(typeof(PortfolioAnalyzer.Areas.Identity.IdentityHostingStartup))]
+namespace PortfolioAnalyzer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ PortfolioAnalyzer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PortfolioAnalyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

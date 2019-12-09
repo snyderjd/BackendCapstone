70c0fb1ad2888a7af65a09f50b6178996757490d
Joseph Snyder <snyderjosephd@yahoo.com>
2019-12-09 17:45:36 -0600

    Added SecuritiesController with Views. Index View shows a list of all securities in the DB with Name and Ticker, Details View shows Name, Ticker, and Description

---
+++ PortfolioAnalyzer/Controllers/SecuritiesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PortfolioAnalyzer.Data;
+using PortfolioAnalyzer.Models;
+
+namespace PortfolioAnalyzer.Controllers
+{
+    public class SecuritiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SecuritiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Securities
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Securities.ToListAsync());
+        }
+
+        // GET: Securities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var security = await _context.Securities
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (security == null) return NotFound();
+
+            return View(security);
+        }
+
+        // GET: Securities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Securities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Ticker,Description")] Security security)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(security);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(security);
+        }
+
+        private bool SecurityExists(int id)
+        {
+            return _context.Securities.Any(e => e.Id == id);
+        }
+    }
+}

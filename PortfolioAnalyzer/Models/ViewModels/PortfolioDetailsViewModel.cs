a9c0343f1676ce97b8cd92f900e931a0d9397d10
Joseph Snyder <snyderjosephd@yahoo.com>
2019-12-11 11:06:06 -0600

    Portfolio DetailsView shows portfolio name and description along with the securities that are in the portfolio

---
+++ PortfolioAnalyzer/Models/ViewModels/PortfolioDetailsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioAnalyzer.Models.ViewModels
+{
+    public class PortfolioDetailsViewModel
+    {
+        public Portfolio Portfolio { get; set; }
+        public decimal Return { get; set; }
+        public decimal CAGR { get; set; }
+        public decimal StdDeviation { get; set; }
+        public List<SelectListItem> TimePeriodOptions { get; }
+        public string TimePeriod { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        public PortfolioDetailsViewModel()
+        {
+            TimePeriodOptions = new List<SelectListItem>()
+            {
+                new SelectListItem("1 Year", "1y"),
+                new SelectListItem("2 Years", "2y"),
+                new SelectListItem("5 Years", "5y"),
+                new SelectListItem("Max", "max")
+            };
+        }
+    }
+}

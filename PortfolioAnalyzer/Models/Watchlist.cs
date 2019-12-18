c88b8692ae29ee6749bd1ad3d62883d57f3c406b
Joseph Snyder <snyderjosephd@yahoo.com>
2019-12-18 11:03:51 -0600

    Created Watchlist and WatchlistSecurity models

---
+++ PortfolioAnalyzer/Models/Watchlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioAnalyzer.Models
+{
+    public class Watchlist
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public virtual ICollection<WatchlistSecurity> WatchlistSecurities { get; set; }
+    }
+}
+

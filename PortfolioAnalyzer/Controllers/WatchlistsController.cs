22a2469ba812cb788b5c3efdd2ec9626dc9e6f02
Joseph Snyder <snyderjosephd@yahoo.com>
2019-12-18 12:04:24 -0600

    Added WatchlistsController and Views and added configuration properties and constructor to WatchlistsController

---
+++ PortfolioAnalyzer/Controllers/WatchlistsController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PortfolioAnalyzer.Data;
+using PortfolioAnalyzer.Models;
+
+namespace PortfolioAnalyzer.Controllers
+{
+    public class WatchlistsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _config;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public WatchlistsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IConfiguration config, IHttpClientFactory clientFactory)
+        {
+            _context = context;
+            _userManager = userManager;
+            _config = config;
+            _clientFactory = clientFactory;
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+        private string GetToken() => _config.GetValue<string>("Tokens:IEXCloudSK");
+
+        public WatchlistsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Watchlists
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Watchlists.Include(w => w.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Watchlists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var watchlist = await _context.Watchlists
+                .Include(w => w.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (watchlist == null)
+            {
+                return NotFound();
+            }
+
+            return View(watchlist);
+        }
+
+        // GET: Watchlists/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
+            return View();
+        }
+
+        // POST: Watchlists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,UserId")] Watchlist watchlist)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(watchlist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", watchlist.UserId);
+            return View(watchlist);
+        }
+
+        // GET: Watchlists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var watchlist = await _context.Watchlists.FindAsync(id);
+            if (watchlist == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", watchlist.UserId);
+            return View(watchlist);
+        }
+
+        // POST: Watchlists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,UserId")] Watchlist watchlist)
+        {
+            if (id != watchlist.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(watchlist);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WatchlistExists(watchlist.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", watchlist.UserId);
+            return View(watchlist);
+        }
+
+        // GET: Watchlists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var watchlist = await _context.Watchlists
+                .Include(w => w.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (watchlist == null)
+            {
+                return NotFound();
+            }
+
+            return View(watchlist);
+        }
+
+        // POST: Watchlists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var watchlist = await _context.Watchlists.FindAsync(id);
+            _context.Watchlists.Remove(watchlist);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WatchlistExists(int id)
+        {
+            return _context.Watchlists.Any(e => e.Id == id);
+        }
+    }
+}

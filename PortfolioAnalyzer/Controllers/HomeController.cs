24ccef5f71eee8bde7340a232b113394efa4905e
Joseph Snyder <snyderjosephd@yahoo.com>
2019-12-29 17:16:26 -0600

    Index method on HomeController takes in a ticker and calls GetFullQuote method if ticker is not null, which returns an IEXHomeQuote that gets assigned to the viewModel

--- PortfolioAnalyzer/Controllers/HomeController.cs
+++ PortfolioAnalyzer/Controllers/HomeController.cs
@@ -28,86 +28,38 @@ namespace PortfolioAnalyzer.Controllers
         }
         private string GetToken() => _config.GetValue<string>("Tokens:IEXCloudSK");
 
-        public IActionResult Index(string ticker)
+        public async Task<IActionResult> Index(string ticker)
         {
-            return View();
+            var viewModel = new HomeViewModel();
+
+            if (ticker != null)
+            {
+                viewModel.Quote = await GetFullQuote(ticker);
+                return View(viewModel);
+            }
+
+            return View(viewModel);
         }
 
-        public async Task<IActionResult> GetFullQuote(string ticker)
+        private async Task<IEXHomeQuote> GetFullQuote(string ticker)
         {
             var token = GetToken();
             var client = _clientFactory.CreateClient();
-            var viewModel = new HomeViewModel();
+            IEXHomeQuote quote = new IEXHomeQuote();
 
-            // If a ticker is passed in, send a request to IEXCloud to retrieve a quote and assign it to the viewModel
-            if (ticker != null)
+            // Send a request to IEXCloud to retrieve a quote and return it
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token={token}");
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
             {
-                var request = new HttpRequestMessage(HttpMethod.Get, $"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token={token}");
-                var response = await client.SendAsync(request);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    // Convert the quote into IEXHomeQuote and add to the list of quotes
-                    var json = await response.Content.ReadAsStreamAsync();
-                    viewModel.Quote = await System.Text.Json.JsonSerializer.DeserializeAsync<IEXHomeQuote>(json);
-                }
-
+                // Convert the quote into IEXHomeQuote and return it
+                var json = await response.Content.ReadAsStreamAsync();
+                quote = await System.Text.Json.JsonSerializer.DeserializeAsync<IEXHomeQuote>(json);
             }
 
-            //foreach (WatchlistSecurity ws in watchlistSecurities)
-            //{
-            //    var request = new HttpRequestMessage(HttpMethod.Get,
-            //        $"https://cloud.iexapis.com/stable/stock/{ws.Security.Ticker}/quote?token={token}");
-            //    var response = await client.SendAsync(request);
-
-            //    if (response.IsSuccessStatusCode)
-            //    {
-            //        // Convert the quote into IEXQuote and add to the list of quotes
-            //        var json = await response.Content.ReadAsStreamAsync();
-            //        IEXQuote quote = await System.Text.Json.JsonSerializer.DeserializeAsync<IEXQuote>(json);
-            //        quotes.Add(quote);
-            //    }
-            //}
-
+            return quote;
         }
-
-        //public async Task<IActionResult> AddSecurity(string newTicker)
-        //{
-        //    var token = GetToken();
-        //    var client = _clientFactory.CreateClient();
-        //    var securities = await _context.Securities.ToListAsync();
-
-        //    // Check to ensure no securities are found in the database with the same ticker, then retrieve from IEXCloud and save to the security table
-        //    if (!securities.Any(s => s.Ticker == newTicker))
-        //    {
-        //        var request = new HttpRequestMessage(HttpMethod.Get,
-        //            $"https://cloud.iexapis.com/stable/stock/{newTicker}/company?token={token}");
-        //        var response = await client.SendAsync(request);
-
-        //        if (response.IsSuccessStatusCode)
-        //        {
-        //            // Convert the response to an object and save the new security to the database
-        //            var json = await response.Content.ReadAsStreamAsync();
-        //            var stockResponse = await System.Text.Json.JsonSerializer.DeserializeAsync<IEXSecurity>(json);
-        //            Security newSecurity = new Security
-        //            {
-        //                Name = stockResponse.CompanyName,
-        //                Ticker = stockResponse.Ticker,
-        //                Description = stockResponse.Description
-        //            };
-
-        //            _context.Securities.Add(newSecurity);
-        //            await _context.SaveChangesAsync();
-
-        //            TempData["SavedMessage"] = "The new security has been added.";
-        //        }
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-
-        //    TempData["SavedMessage"] = "";
-        //    return RedirectToAction(nameof(Index));
-        //}
 
         public IActionResult Privacy()
         {
